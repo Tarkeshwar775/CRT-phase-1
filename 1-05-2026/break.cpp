b42cab19346bc03dc7fcdfce83b05c29148d227b
Tarkeshwar775 <1771tarkeshwar@gmail.com>
2026-05-01 23:52:49 +0530

    Create break.cpp

---
+++ 1-05-2026/break.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    int sum = 0;
+    
+    while(true) {
+       cin >> n;
+       
+       if(n < 0){
+           break;
+       }
+       sum = sum + n;
+    }
+    cout << sum;
+    return 0;
+}
