8cfc2c1ecd6504a546712d443b7ef940b93f0c73
Tarkeshwar775 <1771tarkeshwar@gmail.com>
2026-05-01 23:50:56 +0530

    Create ifelse.cpp

---
+++ 1-05-2026/ifelse.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int age;
+    cin >> age;
+    // if condition 
+    if(age >= 18) {
+        // logic 
+        cout << "Eligible For vote";
+    } else {
+        // logic 
+        cout << "Not Eligible For vote";
+    }
+    return 0;
+}
