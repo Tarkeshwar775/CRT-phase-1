38c34a927498210fb12ceb6ccfa5da7904bf3e4f
Tarkeshwar775 <1771tarkeshwar@gmail.com>
2026-05-01 23:25:10 +0530

    Create sumavg.cpp

---
+++ 30-4-2026/sumavg.cpp
@@ -0,0 +1,16 @@
+include<iostream>
+using namespace std;
+
+int main() {
+    int a, b, c, sum;
+    float avergae;
+    
+    cin >> a >> b >> c;
+    sum = a + b + c;
+    avergae = sum / 3.0;
+    
+    cout << sum << endl;
+    cout << avergae << endl;
+    
+    return 0;
+}
